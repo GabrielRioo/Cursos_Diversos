97d828554f07a7748b03c467fde2aefbc4cb4b85
GabrielRioo <Gabrielpaivario@gmail.com>
2020-08-20 19:05:01 -0300

    Adding webservice

---
+++ Programacao/C#/webService/TutorialSoap/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TutorialSoap")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TutorialSoap")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6afe524d-e9da-4a6e-ae8e-de5786e222ad")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Programacao/C#/webService/WebApplication/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication.ServiceReference1;
+
+namespace WebApplication.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private static List<string> _products { get; set; }
+
+		private static List<string> products
+        {
+			get
+            {
+				if (_products == null)
+                {
+                    _products = new List<string>();
+                }
+                return _products;
+            }
+        }
+
+        public string GetString(string name)
+        {
+            return "Hello " + name;
+        }
+
+        [HttpGet]
+        public bool AddProduct(string name)
+        {
+
+            return new Service1Client().AddProduct(name);
+        }
+
+        [HttpGet]
+        public string[] GetProducts()
+        {
+            return new Service1Client().GetProducts();
+        }
+
+        [HttpGet]
+		public string TestSoap(int value)
+		{
+            return new Service1Client().GetData(value);
+		}
+
+    }
+
+
+}
